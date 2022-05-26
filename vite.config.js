90adf0001aa57419e885e46bb375d0d2f68b443a
Rony-Majzoub <majzoubrony@gmail.com>
2022-05-26 22:31:46 +0200

    Sitemap Test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
 const path = require("path");
 export default defineConfig({
   //...
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Pages({
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
