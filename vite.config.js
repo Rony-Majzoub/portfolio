fb16378320f26e87e680a60443213806425d6559
Rony-Majzoub <majzoubrony@gmail.com>
2022-05-27 02:43:52 +0200

    build test 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import generateSitemap from "vite-plugin-pages-sitemap";
 
 const path = require("path");
-export default defineConfig({
+export default {
   //...
   plugins: [
     vue(),
     Pages({
-      dirs: { dir: "src/pages", baseRoute: "" },
+      dirs: [
+        { dir: "src/pages", baseRoute: "" },
+        { dir: "src/features/**/pages", baseRoute: "features" },
+        { dir: "src/admin/pages", baseRoute: "admin" },
+      ],
       onRoutesGenerated: (routes) => generateSitemap({ routes }),
     }),
   ],
@@ -28,4 +31,4 @@ export default defineConfig({
       },
     },
   },
-});
+};
