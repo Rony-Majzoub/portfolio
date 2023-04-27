c8b607a015796cd27d72214132cf32264627a4d0
Rony-Majzoub <majzoubrony@gmail.com>
2023-04-27 17:07:44 +0200

    Size Fixes

--- src/components/Project/ProjectImageNew.vue
+++ src/components/Project/ProjectImageNew.vue
@@ -20,7 +20,7 @@
             :alt="`${altText}`"
             width="432"
             height="768"
-            class="cld-responsive high-def object-contain w-auto max-w-full h-auto min-h-auto max-h-full relative text-[0] rounded-md sm:rounded-[calc(2.16346vw-6.46154px)] lg:rounded-[.78125vw] shadow-2xl shadow-[rgb(0,0,0)]/30 overflow-hidden" />
+            class="cld-responsive high-def object-contain w-auto max-w-full h-full min-h-auto max-h-full relative text-[0] rounded-md sm:rounded-[calc(2.16346vw-6.46154px)] lg:rounded-[.78125vw] shadow-2xl shadow-[rgb(0,0,0)]/30 overflow-hidden" />
         </div>
       </div>
     </div>
