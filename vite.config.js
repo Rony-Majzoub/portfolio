9b2e5fd0f014526a57b4ca93e9062215165deff2
Rony-Majzoub <majzoubrony@gmail.com>
2023-05-11 10:17:36 +0200

    Use localhost as Dev Adress

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import Components from "unplugin-vue-components/vite";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import { visualizer } from "rollup-plugin-visualizer";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 const config = defineConfig({
   server: {
