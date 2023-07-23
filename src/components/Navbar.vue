6d81903a140f9a7c8f01b6c08388437750fd2368
Rony-Majzoub <majzoubrony@gmail.com>
2023-07-23 15:22:20 +0200

    Navbar Pill Performance Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,17 +3,17 @@
     <div
       class="navbar border-[3px] border-pastel-pink flex col-start-2 col-end-[-2] w-full flex-row justify-evenly gap-4 p-4 py-3 bg-eggplant rounded-full mx-auto shadow-md shadow-[#2b212c]/50 duration-300 ease-in-out">
       <router-link
-        class="nav-mobile inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
+        class="nav-mobile z-10 inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
         :to="{ path: '/', hash: '#projects' }">
         Projects
       </router-link>
       <router-link
-        class="nav-mobile inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
+        class="nav-mobile z-10 inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
         to="/fun">
         Fun
       </router-link>
       <router-link
-        class="nav-mobile inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
+        class="nav-mobile z-10 inline relative transition-all duration-300 py-1 px-2 text-cameo-pink text-lg font-bold text-center antialiased rounded-full whitespace-nowrap"
         to="/about">
         About Me
       </router-link>
@@ -31,8 +31,29 @@ export default {
 
 <style lang="scss" scoped>
 .nav-mobile.router-link-active {
-  background-color: var(--unbleached-silk) !important;
   color: var(--black-coffee) !important;
   transition: var(--animation-curve) 250ms !important;
+}
+.nav-mobile::before {
+  content: "";
+  position: absolute;
+  display: block;
+  background-color: var(--unbleached-silk);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: opacity 250ms;
+  transition-timing-function: var(--animation-curve);
+  will-change: opacity;
+  border-radius: 9999px;
+  z-index: -10;
+}
+
+.nav-mobile.router-link-active::before {
+  opacity: 1;
 }
 </style>
